33123855ac9bf385594da53b18d41ee3597c65a3
Alex V <vtorov.alex@gmail.com>
2024-02-08 17:08:23 +0300

    Update history_uint.sql

--- history_uint.sql
+++ history_uint.sql
@@ -2,6 +2,7 @@ CREATE TABLE history_uint_new (LIKE history_uint INCLUDING DEFAULTS INCLUDING CO
 SELECT create_hypertable('history_uint_new', 'clock', chunk_time_interval => 86400);
 ALTER TABLE IF EXISTS history_uint RENAME TO history_uint_old;
 ALTER TABLE IF EXISTS history_uint_new RENAME TO history_uint;
+GRANT ALL PRIVILEGES ON TABLE history_uint TO zabbix;
 INSERT INTO history_uint SELECT * FROM history_uint_old;
 DROP TABLE IF EXISTS history_uint_old;
 CREATE INDEX history_uint_1 on history_uint (itemid,clock);
