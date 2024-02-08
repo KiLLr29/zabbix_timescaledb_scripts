3c8a931956c92325a96c8c6683256f5a5710ddb0
Alex V <vtorov.alex@gmail.com>
2024-02-08 16:43:38 +0300

    Update history_uint.sql

--- history_uint.sql
+++ history_uint.sql
@@ -1,6 +1,7 @@
 CREATE TABLE history_uint_new (LIKE history_uint INCLUDING DEFAULTS INCLUDING CONSTRAINTS EXCLUDING INDEXES);
 SELECT create_hypertable('history_uint_new', 'clock', chunk_time_interval => 86400);
-INSERT INTO history_uint_new SELECT * FROM history_uint;
-DROP TABLE IF EXISTS history_uint;
+ALTER TABLE IF EXISTS history_uint RENAME TO history_uint_old;
 ALTER TABLE IF EXISTS history_uint_new RENAME TO history_uint;
+INSERT INTO history_uint SELECT * FROM history_uint_old;
+DROP TABLE IF EXISTS history_uint_old;
 CREATE INDEX history_uint_1 on history_uint (itemid,clock);
