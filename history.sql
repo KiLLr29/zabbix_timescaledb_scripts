e60136e5bd31ce48307d94adadf6f1dabb135e51
Alex V <vtorov.alex@gmail.com>
2024-02-09 11:26:03 +0300

    Update history.sql

--- history.sql
+++ history.sql
@@ -1,6 +1,8 @@
 CREATE TABLE history_new (LIKE history INCLUDING DEFAULTS INCLUDING CONSTRAINTS EXCLUDING INDEXES);
 SELECT create_hypertable('history_new', 'clock', chunk_time_interval => 86400);
-INSERT INTO history_new SELECT * FROM history;
-DROP TABLE IF EXISTS history;
+ALTER TABLE IF EXISTS history RENAME TO history_old;
 ALTER TABLE IF EXISTS history_new RENAME TO history;
-CREATE INDEX history_1 on history (itemid,clock);
+CREATE INDEX history_1 on history_uint (itemid,clock);
+GRANT ALL PRIVILEGES ON TABLE history TO zabbix;
+INSERT INTO history SELECT * FROM history_old;
+DROP TABLE IF EXISTS history_old;
